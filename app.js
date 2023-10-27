85fb12c67d5b266fc449c21558ddbf206d9567bc
eklipse Development <46606372+eklipsedev@users.noreply.github.com>
2023-10-27 10:48:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.querySelectorAll("[data-element='lesson']")
   );
   const moduleItems = document.querySelectorAll("[data-element='module']");
-  const courseItems = document.querySelectorAll("[data-course");
   const lessonBtn =
     document.querySelector("[data-element='lesson-button']") || null;
   const currentLessonSlug =
@@ -11,60 +10,44 @@ document.addEventListener("DOMContentLoaded", async () => {
   const currentCourseSlug =
     document.body.getAttribute("data-current-course") || null;
   const memberstack = window.$memberstackDom;
-  let memberId;
+
   let memberJsonData;
   let totalCompletedLessons = 0;
+  let firstIncompleteLesson = null;
 
   memberstack.getCurrentMember().then(async ({ data: member }) => {
     if (member) {
-      memberId = member.id;
-      const memberJson = await memberstack.getMemberJSON(memberId);
-      memberJsonData = memberJson;
-      const completedLessons = memberJson.data.completed_lessons || [];
-      const completedCourses = memberJson.data.courses || [];
-      totalCompletedLessons = completedLessons.length;
-      updateCourseProgress();
-      setupLessonCompletion(
-        lessonItems,
-        completedLessons,
-        currentLessonSlug,
-        memberId
-      );
+      const memberJson = await memberstack.getMemberJSON();
+      memberJsonData = memberJson.data;
+
+      const courses = memberJsonData !== null ? memberJsonData.courses : [];
+
+      if (!courses.length) {
+        memberJsonData = {
+          courses: [{ slug: currentCourseSlug, completed_lessons: [] }]
+        };
+      }
+
+      const currentCourse = memberJsonData.courses.length
+        ? memberJsonData.courses.find(
+            (course) => course.slug === currentCourseSlug
+          )
+        : currentCourseSlug;
+      const completedLessons = currentCourse
+        ? currentCourse.completed_lessons
+        : [];
+
+      updateCourseProgress(courses);
+      setupLessonCompletion(lessonItems, completedLessons, currentLessonSlug);
       setAsideData(lessonItems);
-      setupCourseCompletion(completedCourses);
       setupModuleDropdowns(moduleItems, lessonItems, totalCompletedLessons);
       setDropdowns(moduleItems);
     }
   });
 
-  function setupCourseCompletion(completedCourses) {
-    if (courseItems.length) {
-      courseItems.forEach((course) => {
-        const courseSlug = course.getAttribute("data-course");
-        const status = course.querySelector("[data-status");
-
-        completedCourses.forEach((completedCourse) => {
-          if (
-            completedCourse.slug === courseSlug &&
-            completedCourse.complete === true
-          ) {
-            // course is complete
-            status.setAttribute("data-status", "complete");
-            status.textContent = "complete 🎉";
-          } else if (
-            completedCourse.slug === courseSlug &&
-            completedCourse.complete === false
-          ) {
-            // course is in progress
-            status.setAttribute("data-status", "in-progress");
-            status.textContent = "in progress";
-          } else {
-            // course hasn't been started
-          }
-        });
-      });
-    }
-  }
+  // when the page loads, check all lessons for the given course
+  // if there is a lesson in the JSON that is NOT present on the page, remove it from the JSON
+  // this is because the lesson was removed internally
 
   function setupLessonCompletion(
     lessonItems,
@@ -75,6 +58,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       const lessonSlug = lesson.getAttribute("data-slug");
       if (completedLessons.includes(lessonSlug)) {
         lesson.classList.add("is-complete");
+        totalCompletedLessons++;
         if (currentLessonSlug && lessonSlug === currentLessonSlug) {
           setLessonButtonState(lessonBtn, "add");
         }
@@ -84,9 +68,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (lessonBtn) {
       lessonBtn.addEventListener("click", () => {
         if (lessonBtn.classList.contains("is-complete")) {
-          updateLesson("remove", currentLessonSlug);
+          updateLesson("remove");
+          totalCompletedLessons--;
         } else {
-          updateLesson("add", currentLessonSlug);
+          updateLesson("add");
+          totalCompletedLessons++;
         }
       });
     }
@@ -110,9 +96,13 @@ document.addEventListener("DOMContentLoaded", async () => {
       const trigger = module.querySelector("[data-element='trigger']");
       const dropdown = trigger.parentElement;
       const links = dropdown.querySelectorAll("a");
+      const asideBtn = document.querySelector("[data-aside='button']");
 
       links.forEach((link) => {
-        if (link.classList.contains("w--current")) {
+        if (
+          link.classList.contains("w--current") ||
+          link.href === asideBtn.href
+        ) {
           dropdown.classList.add("is-active");
           return;
         }
@@ -124,48 +114,77 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
   }
 
-  async function updateLesson(type, lessonSlug) {
-    const completedLessons = memberJsonData.data.completed_lessons;
+  async function updateLesson(action) {
+    let currentCourse =
+      memberJsonData.courses.find(
+        (course) => course.slug === currentCourseSlug
+      ) || {};
+
+    if (!currentCourse) {
+      // Handle the case where the course is not found.
+      return;
+    }
+
+    let completedLessons = currentCourse.completed_lessons || [];
+
     // Find all elements with the specified lessonSlug
-    const elements = document.querySelectorAll(`[data-slug='${lessonSlug}']`);
+    const elements = document.querySelectorAll(
+      `[data-slug='${currentLessonSlug}']`
+    );
 
     setLoader(elements);
 
     // Update the completed lessons array based on the type
     let updatedCompletedLessons = completedLessons;
 
-    if (type === "add" && !completedLessons.includes(lessonSlug)) {
-      updatedCompletedLessons = [...completedLessons, lessonSlug];
-    } else if (type === "remove" && completedLessons.includes(lessonSlug)) {
+    if (action === "add" && !completedLessons.includes(currentLessonSlug)) {
+      updatedCompletedLessons = [...completedLessons, currentLessonSlug];
+    } else if (
+      action === "remove" &&
+      completedLessons.includes(currentLessonSlug)
+    ) {
       updatedCompletedLessons = completedLessons.filter(
-        (lesson) => lesson !== lessonSlug
+        (lesson) => lesson !== currentLessonSlug
       );
+    } else {
+      // No update needed
+      return;
     }
 
-    // Update the totalCompletedLessons variable
-    totalCompletedLessons = updatedCompletedLessons.length;
+    // Create a new currentCourse object with the updated completed_lessons property
+    currentCourse = {
+      slug: currentCourseSlug,
+      completed_lessons: updatedCompletedLessons
+    };
+
+    const updatedCourses = memberJsonData.courses.map((course) =>
+      course.slug === currentCourseSlug ? currentCourse : course
+    );
 
     // Update the member's data
     const updatedMemberData = {
-      ...memberJsonData.data,
-      completed_lessons: updatedCompletedLessons
+      ...memberJsonData,
+      courses: updatedCourses
     };
+
+    memberJsonData = updatedMemberData;
+
     const result = await memberstack.updateMemberJSON({
       json: updatedMemberData
     });
 
     // Update the UI for all elements with the specified lessonSlug
     elements.forEach((element) => {
-      if (type === "add") {
+      if (action === "add") {
         element.classList.add("is-complete");
-      } else if (type === "remove") {
+      } else if (action === "remove") {
         element.classList.remove("is-complete");
       }
     });
 
     setLoader(elements);
-    setLessonButtonState(lessonBtn, type);
-    updateCourseProgress();
+    setLessonButtonState(lessonBtn, action);
+    updateCourseProgress(updatedCourses);
   }
 
   function setLessonButtonState(button, type) {
@@ -197,19 +216,37 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
   }
 
-  function updateCourseProgress() {
-    const progressBar =
-      document.querySelector("[data-element='progress']") || null;
-    const progressPercentage = document.querySelector(
-      "[data-element='progress-percentage']" || null
+  function updateCourseProgress(courses) {
+    // assumes there will only be 1 course of each type per page
+    Array.from(document.querySelectorAll("[data-course]")).forEach(
+      (courseElement) => {
+        const element = courseElement.getAttribute("data-element");
+        const slug = courseElement.getAttribute("data-course");
+        const lessonCount = courseElement.getAttribute("data-lesson-count");
+        const currentCourse =
+          courses.find((course) => course.slug === slug) || {};
+        const completedLessons =
+          Object.keys(currentCourse).length > 0
+            ? currentCourse.completed_lessons
+            : [];
+        const percentage = completedLessons.length
+          ? (completedLessons.length / lessonCount) * 100
+          : 0;
+
+        if (element === "progress") {
+          if (percentage === 100) {
+            courseElement.classList.add("is-complete");
+          }
+          courseElement.style.width = percentage + "%";
+        } else if (element === "progress-percentage") {
+          if (percentage === 100) {
+            courseElement.textContent = `${Math.round(percentage)}% 🎉`;
+          } else {
+            courseElement.textContent = `${Math.round(percentage)}%`;
+          }
+        }
+      }
     );
-
-    if (progressBar && progressPercentage) {
-      const percentage = (totalCompletedLessons / lessonItems.length) * 100;
-
-      progressBar.style.width = percentage + "%";
-      progressPercentage.textContent = `${Math.round(percentage)}%`;
-    }
   }
 
   function setAsideData(lessons) {
@@ -220,7 +257,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const currentURL = window.location.href;
 
     if (asideBtn && asideTitle && asideTag) {
-      let firstIncompleteLesson = null;
       let hasAtLeastOneCompleteLesson = false;
 
       for (const lesson of lessons) {
